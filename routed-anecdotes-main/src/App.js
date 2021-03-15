a47b6f0cc3f559bce51514de06160af2765f70ae
Mark Chan <mac101010@gmail.com>
2021-03-15 14:51:20 -0700

    Up to Exercise 7.1
    Added React Router components to form the Menu links and switching between them.

--- routed-anecdotes-main/src/App.js
+++ routed-anecdotes-main/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import {
-  BrowserRouter as Router,
   Switch, Route, Link
 } from "react-router-dom"
 
@@ -10,9 +9,9 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link style={padding} to="/">anecdotes</Link>
+      <Link style={padding} to="/create">create new</Link>
+      <Link style={padding} to="/about">about</Link>
     </div>
   )
 }
@@ -126,13 +125,26 @@ const App = () => {
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
 
+  // for Switch component, the order of components is important
+  // if the path for "/" is first, nothing below it would get rendered
+  // because the "/" is the start of every path, so place it at the very end
   return (
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
+      
+      <Switch>
+        <Route path="/create">
+          <CreateNew addNew={addNew} />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <AnecdoteList anecdotes={anecdotes} />
+        </Route>
+      </Switch>   
+      
       <Footer />
     </div>
   )
